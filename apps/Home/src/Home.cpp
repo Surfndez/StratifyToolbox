23fadb1381471dcd27753cc760cb3a7112e15dc6
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-01-07 20:06:46 -0700

    updating home scene

--- apps/Home/src/Home.cpp
+++ apps/Home/src/Home.cpp
@@ -1,6 +1,7 @@
 #include "Home.hpp"
 #include "Application.hpp"
 
+#include <sapi/ux.hpp>
 #include <sapi/ux/Button.hpp>
 #include <sapi/ux/ProgressBar.hpp>
 #include <sapi/ux/ToggleSwitch.hpp>
@@ -13,7 +14,7 @@ Home::Home(Application & application)
             "ToggleSwitch",
             &(* new ux::ToggleSwitch())
             .set_state(true)
-            .set_drawing_point(ux::DrawingPoint(800,50))
+            .set_drawing_point(ux::DrawingPoint(20,800))
             .set_drawing_area(ux::DrawingArea(200,150))
             .set_event_handler(
                [&](ux::Component * object, const ux::Event & event){
@@ -26,63 +27,120 @@ Home::Home(Application & application)
                   );
                }
 
-   })
+            })
 
+         );
+
+   const ux::DrawingArea control_icon_area(200,200);
+   const ux::DrawingPoint control_icon_start(800,10);
+
+   add_component(
+            "WifiButton",
+            &(* new ux::Button())
+            .set_border_size(1)
+            .set_icon_name("battery-half")
+            .set_theme_style(Theme::style_light)
+            .set_drawing_point(control_icon_start)
+            .set_drawing_area(control_icon_area)
             );
 
    add_component(
-            "Button",
+            "LaunchButton",
+            &(* new ux::Button())
+            .set_border_size(1)
+            .set_icon_name("star")
+            .set_theme_style(Theme::style_light)
+            .set_drawing_point(control_icon_start + ux::DrawingPoint::Y(control_icon_area.height()))
+            .set_drawing_area(control_icon_area)
+            );
+
+   add_component(
+            "EjectButton",
+            &(* new ux::Button())
+            .set_border_size(1)
+            .set_icon_name("bars")
+            .set_theme_style(Theme::style_light)
+            .set_drawing_point(control_icon_start + ux::DrawingPoint::Y(2*control_icon_area.height()))
+            .set_drawing_area(control_icon_area)
+            );
+
+   add_component(
+            "EjectButton",
+            &(* new ux::Button())
+            .set_border_size(1)
+            .set_icon_name("toolbox")
+            .set_theme_style(Theme::style_light)
+            .set_drawing_point(control_icon_start + ux::DrawingPoint::Y(3*control_icon_area.height()))
+            .set_drawing_area(control_icon_area)
+            );
+
+   add_component(
+            "SettingsButton",
+            &(* new ux::Button())
+            .set_border_size(1)
+            .set_icon_name("toolbox")
+            .set_theme_style(Theme::style_light)
+            .set_drawing_point(control_icon_start + ux::DrawingPoint::Y(4*control_icon_area.height()))
+            .set_drawing_area(control_icon_area)
+            );
+
+   add_component(
+            "Button1",
             &(* new ux::Button())
             .set_label("1")
             .set_theme_style(Theme::style_brand_primary)
-            .set_drawing_point(ux::DrawingPoint(0,0))
-            .set_drawing_area(ux::DrawingArea(200,200))
+            .set_drawing_point(ux::DrawingPoint(50,50))
+            .set_drawing_area(ux::DrawingArea(200,600))
             );
 
    add_component(
-            "Button",
+            "Button2",
             &(* new ux::Button())
-            .set_label("2")
-            .set_theme_style(Theme::style_brand_secondary)
-            .set_drawing_point(ux::DrawingPoint(350,0))
+            .set_border_size(5)
+            .set_icon_name("ban")
+            .set_theme_style(Theme::style_light)
+            .set_drawing_point(ux::DrawingPoint(350,50))
             .set_drawing_area(ux::DrawingArea(300,300))
             );
 
    add_component(
-            "Button",
-            &(* new ux::Button())
-            .set_label("info")
-            .set_theme_style(Theme::style_info)
-            .set_drawing_point(ux::DrawingPoint(350,350))
-            .set_drawing_area(ux::DrawingArea(300,300))
-            );
-
-   add_component(
-            "Button",
-            &(* new ux::Button())
-            .set_label("yes")
-            .set_theme_style(Theme::style_success)
-            .set_drawing_point(ux::DrawingPoint(0,350))
-            .set_drawing_area(ux::DrawingArea(300,300))
-            );
-
-   add_component(
-            "Button",
-            &(* new ux::Button())
-            .set_label("no")
+            "Slider1",
+            &(* new ux::Slider())
+            .set_value(25)
+            .set_maximum(100)
             .set_theme_style(Theme::style_danger)
-            .set_drawing_point(ux::DrawingPoint(0,700))
-            .set_drawing_area(ux::DrawingArea(300,300))
-            );
+            .set_drawing_point(ux::DrawingPoint(100,400))
+            .set_drawing_area(ux::DrawingArea(400,100))
+            .set_event_handler(
+               [&](ux::Component * object, const ux::Event & event){
+
+               if( event.type() == ux::SliderEvent::event_type() ){
+                  ux::ProgressBar * progress_bar = find_component<ux::ProgressBar>("Progress1");
+                  if( progress_bar ){
+                     application.printer().info("update progress bar %d/%d",
+                     event.reinterpret<ux::SliderEvent>().value(),
+                     event.reinterpret<ux::SliderEvent>().maximum()
+                     );
+                     progress_bar->set_value( event.reinterpret<ux::SliderEvent>().value() );
+                     progress_bar->set_maximum( event.reinterpret<ux::SliderEvent>().maximum() );
+                     progress_bar->redraw();
+                  }
+               }
+
+            })
+         );
 
    add_component(
-            "Button",
-            &(* new ux::Button())
-            .set_label("warn")
-            .set_theme_style(Theme::style_warning)
-            .set_drawing_point(ux::DrawingPoint(350,675))
-            .set_drawing_area(ux::DrawingArea(300,300))
+            "Progress1",
+            &(* new ux::ProgressBar())
+            .set_border_thickness(50)
+            .set_value(25)
+            .set_maximum(100)
+            .set_theme_style(Theme::style_brand_secondary)
+            .set_drawing_point(ux::DrawingPoint(100,550))
+            .set_drawing_area(ux::DrawingArea(400,50))
             );
+
 
 }
 
