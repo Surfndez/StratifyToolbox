8acbf2c6c1aa42ffb5df63b90cd174b76aa7532f
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-01-27 23:14:44 -0700

    GpioTool updates

--- apps/GpioTool/src/Control.cpp
+++ apps/GpioTool/src/Control.cpp
@@ -4,6 +4,7 @@
 #include <sapi/var.hpp>
 #include <sapi/sys.hpp>
 #include <sapi/ux.hpp>
+#include <ToolboxAPI/toolbox.hpp>
 
 Control::Control(Application & application)
 	: toolbox::ApplicationScene<Application>(application){
@@ -31,40 +32,57 @@ Control::Control(Application & application)
 
 
 				})
-				);
+			);
 
-	const u32 rows = 3;
 	const u32 columns = 4;
 
-	for(u32 row=0; row < rows; row++){
-		for(u32 column=0; column < columns; column++){
-			u32 pin = row*columns + column;
-			String label;
-			label.format("io%d", pin);
-			add_component(
-						label,
-						(* new ux::Button())
-						.set_border_size(1)
-						.set_label(label)
-						.set_theme_style(static_cast<Theme::style>(pin % Theme::last_style))
-						.set_drawing_point(DrawingPoint(column*250+25, 200 + row*225+25))
-						.set_drawing_area(DrawingArea(200, 175))
-						);
-		}
-	}
+	var::Vector<toolbox::IoInformation> io_information_list =
+			toolbox::Io::io_information_list(
+				toolbox::IoInformation::type_io
+				);
 
+	u32 row = 0;
+	u32 column = 0;
+
+	const DrawingArea button_area(225,200);
 	const DrawingArea pin_area(45,40);
-	const u32 pins = 20;
-	for(u32 pin=0; pin < pins; pin++){
+
+	for(const auto & info: io_information_list){
+		u16 pin = info.physical_pin_number();
+		enum Theme::style style =
+				static_cast<Theme::style>(pin % Theme::last_style);
+
+		add_component(
+					info.name(),
+					(* new ux::Button())
+					.set_border_size(1)
+					.set_label(info.name())
+					.set_theme_style(style)
+					.set_drawing_point(
+						DrawingPoint(
+							column*(button_area.width()+25)+25/2,
+							200 + row*(button_area.height()+25)+25
+							)
+						)
+					.set_drawing_area(button_area)
+					);
+
 		add_component(
 					String().format("%d", pin),
 					(* new ux::Label())
 					.set_border_size(1)
-					.set_theme_style(static_cast<Theme::style>(pin % Theme::last_style))
-					.set_drawing_point(DrawingPoint(pin*pin_area.width()+50, 1000-pin_area.height()))
+					.set_theme_style(style)
+					.set_drawing_point(DrawingPoint((pin-1)*pin_area.width()+50, 1000-pin_area.height()))
 					.set_drawing_area(pin_area)
 					);
+
+		column++;
+		if( column == columns ){
+			row++;
+			column = 0;
+		}
 	}
+
 
 
 
