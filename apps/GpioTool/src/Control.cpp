a3af4becfd39f3190df1a1b59068527466b9c3e9
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-02-20 17:33:16 -0700

    GpioTool Control updates

--- apps/GpioTool/src/Control.cpp
+++ apps/GpioTool/src/Control.cpp
@@ -35,6 +35,24 @@ Control::Control(Application & application)
 	const DrawingArea button_area(225,200);
 	const DrawingArea pin_area(45,40);
 
+	add_component(
+				"PinMarkerBar",
+				(* new PinMarkerBar(
+					 event_loop(),
+					 IoInformation::type_io
+					 )
+				 )
+				//bottom 10% of screen (full width)
+				.set_drawing_point(DrawingPoint(0,900))
+				.set_drawing_area(DrawingArea(1000,100))
+				);
+
+	PinMarkerBar * pin_marker_bar = find<PinMarkerBar>("PinMarkerBar");
+	if( pin_marker_bar == nullptr ){
+		printf("pin marker bar is null\n");
+		exit(1);
+	}
+
 	for(const auto & info: io_information_list){
 		u16 pin = info.physical_pin_number();
 		enum Theme::style style =
@@ -55,24 +73,17 @@ Control::Control(Application & application)
 					.set_drawing_area(button_area)
 					);
 
+		pin_marker_bar->set_pin_marker_style(
+					info.io_pin(),
+					style
+					);
+
 		column++;
 		if( column == columns ){
 			row++;
 			column = 0;
 		}
 	}
-
-	add_component(
-				"PinMarkerBar",
-				(* new PinMarkerBar(
-					 event_loop(),
-					 IoInformation::type_io
-					 )
-				 )
-				//bottom 10% of screen (full width)
-				.set_drawing_point(DrawingPoint(0,900))
-				.set_drawing_area(DrawingArea(1000,100))
-				);
 
 	set_event_handler(Control::handle_event);
 
@@ -104,9 +115,21 @@ void Control::handle_event(
 								info.io_pin(),
 								io.value()
 								);
+
+					Button * button = control->find<Button>(info.name());
+					if( button != nullptr ){
+
+						pin_marker_bar->set_pin_marker_state(
+									info.io_pin(), button->theme_state()
+									);
+					}
+
 				} else {
 					//assert
 				}
+
+
+
 			}
 		}
 
